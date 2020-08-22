09246c878a6f44e4140a744e6541fad26ea4d40c
Alex <alexsmekhunov@gmail.com>
2020-08-22 22:29:25 +0300

    Added admin panel for models

--- restful_server/admin.py
+++ restful_server/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
+from django.contrib.gis import admin
+from .models import PointModel, LineModel
 # Register your models here.
+
+admin.site.register(PointModel, admin.GeoModelAdmin)
+admin.site.register(LineModel, admin.ModelAdmin)
--- restful_server/models.py
+++ restful_server/models.py
@@ -7,7 +7,13 @@ class PointModel(models.Model):
     geom = models.PointField()
     score = models.IntegerField()
 
+    def __str__(self):
+        return "%d: (%f, %f) Sc: %d" % (self.id, self.geom.coords[0], self.geom.coords[1], self.score)
+
 
 class LineModel(models.Model):
     from_point = models.ForeignKey(to='PointModel', on_delete=models.PROTECT, related_name='from_point')
     to_point = models.ForeignKey(to='PointModel', on_delete=models.PROTECT, related_name='to_point')
+
+    def __str__(self):
+        return str(self.from_point.id) + " -> " + str(self.to_point.id)
